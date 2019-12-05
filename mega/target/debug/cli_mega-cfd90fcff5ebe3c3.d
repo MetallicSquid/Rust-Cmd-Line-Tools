624369d9abdfde4fb4925010c2712c6c126d835b
MetalicSquid <36278739+MetalicSquid@users.noreply.github.com>
2019-12-05 21:15:04 +0000

    Commit for v0.1.1 of mega'

---
+++ mega/target/debug/cli_mega-cfd90fcff5ebe3c3.d
@@ -0,0 +1,1 @@
+C:\Users\gcmac\Desktop\rust-cmd-line-tools\mega\target\debug\cli_mega-cfd90fcff5ebe3c3.exe: C:\Users\gcmac\Desktop\rust-cmd-line-tools\mega\src\main.rs
---
+++ mega/target/debug/deps/cli_mega-cfd90fcff5ebe3c3.d
@@ -0,0 +1,5 @@
+C:\Users\gcmac\Desktop\rust-cmd-line-tools\mega\target\debug\deps\cli_mega-cfd90fcff5ebe3c3.exe: src\main.rs
+
+C:\Users\gcmac\Desktop\rust-cmd-line-tools\mega\target\debug\deps\cli_mega-cfd90fcff5ebe3c3.d: src\main.rs
+
+src\main.rs:
